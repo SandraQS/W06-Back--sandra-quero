59fd2199cb4f01d0ea62604e85e3d48b52898cf7
SandraQS <sanquesan@gmail.com>
2021-11-09 20:13:53 +0100

    Modify index database with new Promise

--- database/index.js
+++ database/index.js
@@ -2,15 +2,22 @@ const chalk = require("chalk");
 const mongoose = require("mongoose");
 const debug = require("debug")("robots:database");
 
-const initDB = () => {
-  mongoose.connect(process.env.MONGODB_STRING_ROBOTS, (error) => {
-    if (error) {
-      debug(chalk.red("No se ha podido conectar con la base de datos... :("));
-      debug(chalk.red(error.message));
-      return;
-    }
-    debug(chalk.green("Se ha conectado con la base de datos"));
+const initDB = (conectionDBString) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(conectionDBString, (error) => {
+      if (error) {
+        debug(chalk.red("No se ha podido conectar con la base de datos... :("));
+        debug(chalk.red(error.message));
+        reject();
+        return;
+      }
+      debug(chalk.green("Se ha conectado con la base de datos"));
+      resolve();
+    });
+
+    mongoose.conection.on("close", () => {
+      debug(chalk.green("La base de datos se ha desconectado"));
+    });
   });
-};
 
 module.exports = initDB;
